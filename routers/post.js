f9db14382cbb32863d27a112b4e3ff8ad9a03065
yzkim9501 <yzkim9501@naver.com>
2021-07-16 15:48:56 +0900

    post db 불러오기용 테스트 코드

--- routers/post.js
+++ routers/post.js
@@ -2,8 +2,10 @@ const express = require("express");
 const router = express.Router();//라우터라고 선언한다.
 const Post = require("../schemas/post");
 
-router.get("", async (req, res, next) => {
-
+router.get("/all", async (req, res, next) => {
+    const posts = await Post.find({});
+    
+      res.json({ posts: posts });
 });
 
 module.exports = router;
